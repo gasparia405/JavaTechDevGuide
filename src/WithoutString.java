5dcd1e0dce99e77e8de1b4fe921647bb7eb8b99b
Andrew Gaspari <gasparia405@gmail.com>
2018-12-30 22:31:00 -0600

    Add README, WithoutString works in most cases, fails in some important cases

--- src/WithoutString.java
+++ src/WithoutString.java
@@ -1,23 +1,35 @@
 public class WithoutString {
 
     public static void main(String[] args) {
-        System.out.println(WithoutString("quick brown fox jumps over the lazy dog", "o"));
+        System.out.println(WithoutString("banana", "ana"));
     }
 
     /**
-     * Takes an string as input and removes the letter specified for each occurrence of the letter in the input string
+     * Takes an string as input and removes the specified letters for each occurrence of the letters in the input string
      *
-     * @param s A string that you want to remove the 
-     * @param charToRemove
-     * @return
+     * @param base A String that you want to remove the letters from
+     * @param remove A String that you want to remove from the input string
+     * @return StringBuilder converted to String that has all requested chars removed
      */
-    private static String WithoutString(String s, String charToRemove) {
+    private static String WithoutString(String base, String remove) {
         StringBuilder finalString = new StringBuilder();
 
-        char[] workingString = s.toCharArray();
+        int lengthHelper = remove.length() - 1;
 
-        for (char c : workingString){
-            if (/*c is not equal to remove string*/) {
+        int indexLimiter = base.length() - remove.length();
+
+        for (int i = 0; i <= indexLimiter; i++){
+            String currentSubstring = base.substring(i, i + remove.length());
+            if (!currentSubstring.equals(remove) && finalString.length() == 0) {
+                finalString.append(currentSubstring);
+            } else if (!currentSubstring.equals(remove)) {
+                finalString.append(currentSubstring.substring(lengthHelper));
+            } else {
+                try {
+                    finalString.delete(finalString.length() - lengthHelper, finalString.length());
+                } catch (StringIndexOutOfBoundsException e) {
+                    i += lengthHelper;
+                }
 
             }
         }
